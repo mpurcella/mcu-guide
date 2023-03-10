e49beddfbeb9fb47011e47055931bf43a2bb38ed
Michael Purcella <mjpurcella@gmail.com>
2023-03-10 13:51:00 -0800

    Refactor hero size to dvh rather than vh

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Avengers from '../images/avengers.png';
 
 const Hero = () => {
     return (
-        <section className="relative h-[calc(100vh-64px)]">
+        <section className="relative h-[calc(100dvh-64px)]">
             <div className="flex h-full items-center justify-center">
                 <img
                     src={Avengers}
@@ -11,7 +11,7 @@ const Hero = () => {
                 />
                 <div className="absolute max-w-screen-sm p-20">
                     <h1 className="text-center text-24 font-bold uppercase leading-tight text-grey-100 text-shadow md:text-40">
-                        The <span className="text-black-100 bg-red px-8 text-shadow-none">Ultimate</span> Guide to the
+                        The <span className="bg-red px-8 text-black-100 text-shadow-none">Ultimate</span> Guide to the
                         Marvel Cinematic&nbsp;Universe
                     </h1>
                 </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,7 @@
 import Hero from '../components/Hero';
 
 const Home = () => {
-	return (
-		<>
-			<Hero />
-		</>
-	);
+    return <Hero />;
 };
 
 export default Home;
