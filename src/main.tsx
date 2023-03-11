10d0cf08163c3acb2c603b467d036e88bbbff1d4
Michael Purcella <mjpurcella@gmail.com>
2023-03-10 16:11:50 -0800

    Refactor hero heights

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ const App = () => {
             <ScrollToTop />
             <SkipToContent />
             <Header />
-            <main id="main" className="min-h-[calc(100vh-64px)]">
+            <main id="main">
                 <Outlet />
             </main>
             <Footer />
